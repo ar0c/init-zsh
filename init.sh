645f4765781fb340f2d9888274fd7665800341ca
root <root@vdeb.vdeb.ar0c.com>
2024-10-26 19:42:55 +0800

    u

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
+apt install curl
+apt install git
+apt install zsh -y
 sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
+chsh -s /bin/zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git config --global credential.helper store
+cp -f .p10k.zsh ~
+cp -f .zshrc ~
