901bb3c4a36d6e5e2371c6c9a78c1d139a9fbeeb
root <root@vdeb.vdeb.ar0c.com>
2024-10-26 19:23:48 +0800

    update

--- init.sh
+++ init.sh
@@ -3,3 +3,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git config --global credential.helper store
+cp .oh-my-zsh ~
+cp .p10k.zsh ~
+cp .zshrc ~
